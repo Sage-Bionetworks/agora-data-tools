b23c07451e01f39f16217d28d02a6b8343589127
mattfazza <accmattfazza@gmail.com>
2021-06-09 15:33:59 -0700

    AG-149: finished standard process for a single file

--- agoradatatools/utils.py
+++ agoradatatools/utils.py
@@ -2,9 +2,10 @@ import synapseclient
 import yaml
 import errno
 import sys
+from agoradatatools import extract, transform, load
 
 
-def _login_to_synapse():
+def _login_to_synapse() -> object:
     syn = synapseclient.Synapse()
     syn.login()
     return syn
@@ -27,4 +28,39 @@ def _get_config(config_path: str = None):
         print("Invalid file.  Please provide a valid YAML file.")
         sys.exit(errno.EBADF)
 
-    return config
+    return config[1]['files']
+
+def process_single_file(file_obj: dict):
+    """
+    Puts a single file through the entire ETL process.
+    If process is successful, it will return a string
+    with the Synapse id of the newly loaded file
+    :param file_obj: a file object from the configuration
+    :return: a Synapse id
+    """
+
+    # individual exceptions are defined in each file
+    try:
+        df = extract.get_entity_as_df(syn_id=file_obj['id'], format=file_obj['format'])
+    except Exception as extract_error:
+        print("There was an error extracting " + file_obj['id'])
+        print(extract_error)
+        return
+
+    try:
+        df = transform.standardize_column_names(df=df)
+        df = transform.standardize_values(df=df)
+    except Exception as transform_error:
+        print("There was an error transforming " + file_obj['id'])
+        print(transform_error)
+        return
+
+    try:
+        json_path = load.df_to_json(df=df, filename=file_obj['final_filename'])
+        syn_id = load.load(file_path=json_path, provenance=file_obj['provenance'], destination=file_obj['destination'])
+    except Exception as load_error:
+        print("There was an error loading " + file_obj['id'])
+        print(load_error)
+        return
+
+    return syn_id
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,26 @@
 import pytest
 from agoradatatools import utils
 from synapseclient import Synapse
+from pandas import DataFrame
+
+file_object = {
+    "id": "syn25838546",
+    "format": "table",
+    "final_filename": "teams",
+    "provenance": [],
+    "destination": "syn25871921"
+}
+
 
 def test_login():
     assert type(utils._login_to_synapse()) is Synapse
 
+
 def test_yaml():
-    #tests if a valid file renders a list
+    # tests if a valid file renders a list
     assert type(utils._get_config()) is list
 
-    #tests if a bad file will
+    # tests if a bad file will
     with pytest.raises(SystemExit) as err:
         utils._get_config(config_path="./tests/test_assets/bad_config.yaml")
     assert err.type == SystemExit
@@ -19,6 +30,12 @@ def test_yaml():
         utils._get_config(config_path="./tests/test_assets/bad_config.yam")
     assert err.type == SystemExit
     assert err.value.code == 2
+
+
+def test_process_single_file():
+    result = utils.process_single_file(file_object)
+    assert type(result) is str
+
 
 if __name__ == "__main__":
     pytest.main()
