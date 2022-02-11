AG-349: fixing floating point precision error

agoradatatools/etl/load.py
@@ -3,6 +3,18 @@ import json
 from os import mkdir, rmdir
 from . import utils
 from synapseclient import File, Activity
+import numpy as np
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
 
 
 def create_temp_location():
@@ -66,10 +78,15 @@ def df_to_json(df: pd.core.frame.DataFrame, filename: str):
     """
 
     try:
+        df = df.replace({np.nan: None})
         temp_json = open("./staging/" + filename, 'w+')
-        json_str = df.to_json(orient='records', indent=2)
-        json_parsed = json.loads(json_str)
-        json.dump(json_parsed, temp_json, indent=2)
+        # json_str = df.
+        # json_str = json.dumps(df.to_dict())
+        # json_parsed = json.loads(json_str)
+        # json.dump(json_parsed, temp_json, indent=2)
+        json.dump(df.to_dict(orient='records'), temp_json,
+                  cls=NumpyEncoder,
+                  indent=2)
     except AttributeError:
         print("Invalid dataframe.")
         return None