a104afd1bbbce5192b37d5b18753cc3278269495
mattfazza <accmattfazza@gmail.com>
2022-06-10 15:06:45 -0700

    adding testing file

---
+++ agoradatatools/etl/test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def describe_dataset(df: pd.DataFrame) -> str:
+    '''
+     place holder for a function to describe datasets. We'll add tests and filter results from describe
+    '''
+    print(df.describe())
