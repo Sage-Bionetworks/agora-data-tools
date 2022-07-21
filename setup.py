5d721810463ff2d4e14213bb356b91833359d2a7
thomasyu888 <thomas.yu@sagebase.org>
2022-07-20 23:51:43 -0700

    Add setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='agora-data-tools',
-    version='1.0.0',
-    packages=['tests', 'agoradatatools', 'agoradatatools.etl'],
-    url='',
-    license='',
-    author='mfazza',
-    author_email='accmattfazza@gmail.com',
-    description='A collection of tools that can be used to work with Agora\'s data'
-)
+setup()
