f7e157b8bf98b503162b2587a8d3d9f44081ee25
Jessica Britton <jessica.britton@sagebase.org>
2022-12-19 13:53:19 -0800

    Add biodomains dataset and custom transform

--- agoradatatools/etl/transform.py
+++ agoradatatools/etl/transform.py
@@ -128,10 +128,10 @@ def transform_team_info(datasets: dict):
     team_info = datasets['team_info']
     team_member_info = datasets['team_member_info']
 
-    team_member_info = team_member_info.groupby('team')\
+    team_member_info = team_member_info.groupby('team') \
         .apply(lambda x: x[x.columns.difference(['team'])]
                .fillna('')
-               .to_dict(orient='records'))\
+               .to_dict(orient='records')) \
         .reset_index(name="members")
 
     return join_datasets(left=team_info, right=team_member_info, how='left', on='team')
@@ -170,22 +170,22 @@ def transform_gene_info(datasets: dict, adjusted_p_value_threshold, protein_leve
     target_list = datasets['target_list']
     median_expression = datasets['median_expression']
     druggability = datasets['druggability']
-    
+
     # Modify the data before merging
-    
+
     # All genes in this list should have 'is_igap' = True when added to gene_info.
     # Creating the column here automatically adds the column in to gene_info 
     # during merge, with True values correctly populated.
-    igap['is_igap'] = True 
-    
+    igap['is_igap'] = True
+
     # Get the smallest adj_p_val for each gene, to determine significance
     rna_change = rna_change.groupby('ensembl_gene_id')['adj_p_val'].agg('min').reset_index()
-    
+
     # Get the smallest cor_pval for each protein, to determine significance
     proteomics_concat = pd.concat([proteomics, proteomics_tmt])
     proteomics_concat = proteomics_concat.dropna(subset=['log2_fc', 'cor_pval', 'ci_lwr', 'ci_upr'])
     proteomics_concat = proteomics_concat.groupby('ensembl_gene_id')['cor_pval'].agg('min').reset_index()
-    
+
     # these are the interesting columns of the druggability dataset
     useful_columns = ['geneid', 'sm_druggability_bucket', 'safety_bucket', 'abability_bucket', 'pharos_class',
                       'classification', 'safety_bucket_definition', 'abability_bucket_definition']
@@ -205,53 +205,52 @@ def transform_gene_info(datasets: dict, adjusted_p_value_threshold, protein_leve
     druggability.rename(columns={'geneid': 'ensembl_gene_id'}, inplace=True)
 
     # Merge all the datasets
-    
+
     gene_info = gene_metadata
-    
+
     for dataset in [igap, eqtl, rna_change, proteomics_concat, target_list, median_expression, druggability]:
         gene_info = pd.merge(left=gene_info,
                              right=dataset,
                              on='ensembl_gene_id',
                              how='outer',
-                             validate = 'one_to_one')
-    
+                             validate='one_to_one')
+
     # Populate values for rows that didn't exist in the individual datasets
-    
+
     gene_info.fillna({'is_igap': False,
                       'has_eqtl': False,
                       'adj_p_val': -1,
-                      'cor_pval': -1}, inplace = True)
-    
+                      'cor_pval': -1}, inplace=True)
+
     # fillna doesn't work for creating an empty array, need this function instead
     gene_info['alias'] = gene_info.apply(
         lambda row: row['alias'] if isinstance(row['alias'], np.ndarray) \
-            else np.ndarray(0, dtype=object), axis = 1)
+            else np.ndarray(0, dtype=object), axis=1)
 
     gene_info['rna_brain_change_studied'] = (gene_info['adj_p_val'] != -1)
     gene_info['rna_in_ad_brain_change'] = (gene_info['adj_p_val'] <= adjusted_p_value_threshold)
 
     gene_info['protein_brain_change_studied'] = (gene_info['cor_pval'] != -1)
     gene_info['protein_in_ad_brain_change'] = (gene_info['cor_pval'] <= protein_level_threshold)
-        
+
     # create 'nominations' field
     gene_info['nominations'] = gene_info.apply(
         lambda row: len(row['nominated_target']) if isinstance(row['nominated_target'], list) else np.NaN, axis=1)
 
     # Remove some extra columns that got added during merges
     gene_info = gene_info[
-        ['ensembl_gene_id', 'name', 'summary', 'symbol', 'alias', 'is_igap', 
-         'has_eqtl', 'rna_in_ad_brain_change', 'rna_brain_change_studied', 
+        ['ensembl_gene_id', 'name', 'summary', 'symbol', 'alias', 'is_igap',
+         'has_eqtl', 'rna_in_ad_brain_change', 'rna_brain_change_studied',
          'protein_in_ad_brain_change', 'protein_brain_change_studied',
          'nominated_target', 'median_expression', 'druggability', 'nominations']]
-    
+
     # Make sure there are no N/A Ensembl IDs
     gene_info = gene_info.dropna(subset=['ensembl_gene_id'])
-    
+
     return gene_info
 
 
 def transform_distribution_data(datasets: dict, overall_max_score, genetics_max_score, omics_max_score, lit_max_score):
-
     overall_scores = datasets['overall_scores']
     interesting_columns = ['ensg', 'overall', 'geneticsscore', 'omicsscore', 'literaturescore']
 
@@ -261,7 +260,8 @@ def transform_distribution_data(datasets: dict, overall_max_score, genetics_max_
     mapping['overall'] = None
 
     # create mapping for max score values from config
-    max_score = dict(zip(interesting_columns[1:], [overall_max_score, genetics_max_score, omics_max_score, lit_max_score]))
+    max_score = dict(
+        zip(interesting_columns[1:], [overall_max_score, genetics_max_score, omics_max_score, lit_max_score]))
 
     overall_scores = overall_scores[interesting_columns + scored]
 
@@ -291,7 +291,8 @@ def transform_rna_distribution_data(datasets: dict):
     rna_df = datasets['rna']
     rna_df = rna_df[['tissue', 'model', 'logfc']]
 
-    rna_df = rna_df.groupby(['tissue', 'model']).agg('describe')['logfc'].reset_index()[['model', 'tissue', 'min', 'max', '25%', '50%', '75%']]
+    rna_df = rna_df.groupby(['tissue', 'model']).agg('describe')['logfc'].reset_index()[
+        ['model', 'tissue', 'min', 'max', '25%', '50%', '75%']]
     rna_df.rename(columns={'25%': 'first_quartile', '50%': 'median', '75%': 'third_quartile'}, inplace=True)
 
     rna_df['IQR'] = rna_df['third_quartile'] - rna_df['first_quartile']
@@ -333,7 +334,6 @@ def transform_proteomics_distribution_data(proteomics_df: pd.DataFrame, datatype
 
 
 def create_proteomics_distribution_data(datasets: dict) -> pd.DataFrame:
-
     transformed = []
     for name, dataset in datasets.items():
         if name == 'proteomics':
@@ -344,8 +344,21 @@ def create_proteomics_distribution_data(datasets: dict) -> pd.DataFrame:
     return pd.concat(transformed)
 
 
-def apply_custom_transformations(datasets: dict, dataset_name: str, dataset_obj: dict):
+def transform_biodomains(datasets: dict):
+    biodomains = datasets['biodomains']
+    interesting_columns = ['ensembl_id', 'biodomain', 'goterms']
+    biodomains = biodomains[interesting_columns]
 
+    biodomains = biodomains.groupby(['ensembl_id', 'biodomain'])['goterms'].apply(list).reset_index()
+
+    biodomains = nest_fields(df=biodomains,
+                             grouping='ensembl_id',
+                             new_column='gene_biodomains')
+
+    return biodomains
+
+
+def apply_custom_transformations(datasets: dict, dataset_name: str, dataset_obj: dict):
     if type(datasets) is not dict or type(dataset_name) is not str:
         return None
 
@@ -355,7 +368,8 @@ def apply_custom_transformations(datasets: dict, dataset_name: str, dataset_obj:
     elif dataset_name == "distribution_data":
         return transform_distribution_data(datasets=datasets,
                                            overall_max_score=dataset_obj['custom_transformations']['overall_max_score'],
-                                           genetics_max_score=dataset_obj['custom_transformations']['genetics_max_score'],
+                                           genetics_max_score=dataset_obj['custom_transformations'][
+                                               'genetics_max_score'],
                                            omics_max_score=dataset_obj['custom_transformations']['omics_max_score'],
                                            lit_max_score=dataset_obj['custom_transformations']['lit_max_score'])
     elif dataset_name == "team_info":
@@ -364,11 +378,15 @@ def apply_custom_transformations(datasets: dict, dataset_name: str, dataset_obj:
         return transform_rna_seq_data(datasets=datasets)
     elif dataset_name == 'gene_info':
         return transform_gene_info(datasets=datasets,
-                                   adjusted_p_value_threshold=dataset_obj['custom_transformations']['adjusted_p_value_threshold'],
-                                   protein_level_threshold=dataset_obj['custom_transformations']['protein_level_threshold'])
+                                   adjusted_p_value_threshold=dataset_obj['custom_transformations'][
+                                       'adjusted_p_value_threshold'],
+                                   protein_level_threshold=dataset_obj['custom_transformations'][
+                                       'protein_level_threshold'])
     elif dataset_name == 'rna_distribution_data':
         return transform_rna_distribution_data(datasets=datasets)
     elif dataset_name == 'proteomics_distribution_data':
         return create_proteomics_distribution_data(datasets=datasets)
+    elif dataset_name == 'biodomains':
+        return transform_biodomains(datasets=datasets)
     else:
         return None
