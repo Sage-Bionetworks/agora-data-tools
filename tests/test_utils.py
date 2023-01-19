bb70cf0768cd49fa89467160dd6329ad268b6934
Brad Macdonald <bwm03570@gmail.com>
2023-01-19 13:12:53 -0700

    fix test get config, remove open(), and yaml mocking

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -49,34 +49,14 @@ def test_get_config_with_scanner_error():
         utils._get_config(config_path="./tests/test_assets/bad_config_scanning.yaml")
 
 
-class TestGetConfigWithAndWithoutPath:
-    def setup_method(self):
-        self.patch_open = patch.object(
-            builtins, "open", return_value=io.TextIOWrapper
-        ).start()
-        self.patch_yaml_load = patch.object(
-            yaml, "load", return_value=[{"a": "b"}, {"c", "d"}]
-        ).start()
+def test_get_config_with_no_config_path():
+    config = utils._get_config(config_path=None)
+    assert next(iter(config)) == {"destination": "syn12177492"}
 
-    def teardown_method(self):
-        self.patch_open.stop()
-        self.patch_yaml_load.stop()
 
-    def test_get_config_with_no_config_path(self):
-        config = utils._get_config(config_path=None)
-        self.patch_open.assert_called_once_with("./config.yaml", "r")
-        self.patch_yaml_load.assert_called_once_with(
-            io.TextIOWrapper, Loader=yaml.FullLoader
-        )
-        assert config == self.patch_yaml_load.return_value
-
-    def test_get_config_with_config_path(self):
-        config = utils._get_config(config_path="this/is/a/good/path")
-        self.patch_open.assert_called_once_with("this/is/a/good/path", "r")
-        self.patch_yaml_load.assert_called_once_with(
-            io.TextIOWrapper, Loader=yaml.FullLoader
-        )
-        assert config == self.patch_yaml_load.return_value
+def test_get_config_with_config_path():
+    config = utils._get_config(config_path="./test_config.yaml")
+    assert next(iter(config)) == {"destination": "syn17015333"}
 
 
 def test_find_config_by_name_where_name_in_config():
