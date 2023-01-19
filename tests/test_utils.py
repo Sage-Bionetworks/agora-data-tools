d5ea6284acc5a1cd7ef1da6eee0fb10e41ae5780
Brad Macdonald <bwm03570@gmail.com>
2023-01-19 11:58:47 -0700

    fix import order

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,12 @@
-import pytest
-from agoradatatools.etl import utils
-from unittest.mock import patch
-import synapseclient
-import yaml
-import io
 import builtins
+import io
+
+import pytest
+import synapseclient
+from unittest.mock import patch
+import yaml
+
+from agoradatatools.etl import utils
 
 
 class TestLoginToSynapse:
@@ -47,34 +49,34 @@ def test_get_config_with_scanner_error():
         utils._get_config(config_path="./tests/test_assets/bad_config_scanning.yaml")
 
 
-# class TestGetConfigWithAndWithoutPath:
-#     def setup_method(self):
-#         self.patch_open = patch.object(
-#             builtins, "open", return_value=io.TextIOWrapper
-#         ).start()
-#         self.patch_yaml_load = patch.object(
-#             yaml, "load", return_value=[{"a": "b"}, {"c", "d"}]
-#         ).start()
+class TestGetConfigWithAndWithoutPath:
+    def setup_method(self):
+        self.patch_open = patch.object(
+            builtins, "open", return_value=io.TextIOWrapper
+        ).start()
+        self.patch_yaml_load = patch.object(
+            yaml, "load", return_value=[{"a": "b"}, {"c", "d"}]
+        ).start()
 
-#     def teardown_method(self):
-#         self.patch_open.stop()
-#         self.patch_yaml_load.stop()
+    def teardown_method(self):
+        self.patch_open.stop()
+        self.patch_yaml_load.stop()
 
-#     def test_get_config_with_no_config_path(self):
-#         config = utils._get_config(config_path=None)
-#         self.patch_open.assert_called_once_with("./config.yaml", "r")
-#         self.patch_yaml_load.assert_called_once_with(
-#             io.TextIOWrapper, Loader=yaml.FullLoader
-#         )
-#         assert config == self.patch_yaml_load.return_value
+    def test_get_config_with_no_config_path(self):
+        config = utils._get_config(config_path=None)
+        self.patch_open.assert_called_once_with("./config.yaml", "r")
+        self.patch_yaml_load.assert_called_once_with(
+            io.TextIOWrapper, Loader=yaml.FullLoader
+        )
+        assert config == self.patch_yaml_load.return_value
 
-#     def test_get_config_with_config_path(self):
-#         config = utils._get_config(config_path="this/is/a/good/path")
-#         self.patch_open.assert_called_once_with("this/is/a/good/path", "r")
-#         self.patch_yaml_load.assert_called_once_with(
-#             io.TextIOWrapper, Loader=yaml.FullLoader
-#         )
-#         assert config == self.patch_yaml_load.return_value
+    def test_get_config_with_config_path(self):
+        config = utils._get_config(config_path="this/is/a/good/path")
+        self.patch_open.assert_called_once_with("this/is/a/good/path", "r")
+        self.patch_yaml_load.assert_called_once_with(
+            io.TextIOWrapper, Loader=yaml.FullLoader
+        )
+        assert config == self.patch_yaml_load.return_value
 
 
 def test_find_config_by_name_where_name_in_config():
