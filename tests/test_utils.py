c92b793335360979cc988f340ff41034df676635
Brad Macdonald <bwm03570@gmail.com>
2023-01-19 15:05:11 -0700

    test order of functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,87 +9,65 @@ import yaml
 from agoradatatools.etl import utils
 
 
-# class TestLoginToSynapse:
-#     @pytest.fixture(scope="function", autouse=True)
-#     def setup_method(self, syn):
-#         self.patch_synapseclient = patch.object(
-#             synapseclient, "Synapse", return_value=syn
-#         ).start()
-#         self.patch_syn_login = patch.object(syn, "login", return_value=syn).start()
-
-#     def teardown_method(self):
-#         self.patch_synapseclient.stop()
-#         self.patch_syn_login.stop()
-
-#     def test_login_with_token(self):
-#         utils._login_to_synapse(token="my_auth_token")
-#         self.patch_synapseclient.assert_called_once()
-#         self.patch_syn_login.assert_called_once_with(authToken="my_auth_token")
-
-#     def test_login_no_token(self):
-#         utils._login_to_synapse(token=None)
-#         self.patch_synapseclient.assert_called_once()
-#         self.patch_syn_login.assert_called_once_with()
+def test_get_config_with_invalid_file_path():
+    with pytest.raises(FileNotFoundError, match="File not found. *"):
+        utils._get_config(config_path="this/is/a/bad/path")
 
 
-def test_login_with_token(syn):
-    with patch.object(
-        synapseclient, "Synapse", return_value=syn
-    ) as patch_synapseclient, patch.object(
-        syn, "login", return_value=syn
-    ) as patch_syn_login:
+def test_get_config_with_parser_error():
+    with pytest.raises(
+        yaml.parser.ParserError, match="YAML file unable to be parsed. *"
+    ):
+        utils._get_config(config_path="./tests/test_assets/bad_config_parsing.yaml")
+
+
+def test_get_config_with_scanner_error():
+    with pytest.raises(
+        yaml.scanner.ScannerError, match="YAML file unable to be scanned. *"
+    ):
+        utils._get_config(config_path="./tests/test_assets/bad_config_scanning.yaml")
+
+
+def test_get_config_with_no_config_path():
+    config = utils._get_config(config_path=None)
+    assert list(config)[0] == {"destination": "syn12177492"}
+
+
+def test_get_config_with_config_path():
+    config = utils._get_config(config_path="./test_config.yaml")
+    assert list(config)[0] == {"destination": "syn17015333"}
+
+
+def test_find_config_by_name_where_name_in_config():
+    config = [{"a": "b"}, {"c", "d"}]
+    returned_object = utils._find_config_by_name(config=config, name="a")
+    assert returned_object is not None
+
+
+def test_find_config_by_name_where_name_not_in_config():
+    config = [{"a": "b"}, {"c": "d"}]
+    returned_object = utils._find_config_by_name(config=config, name="z")
+    assert returned_object is None
+
+
+class TestLoginToSynapse:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup_method(self, syn):
+        self.patch_synapseclient = patch.object(
+            synapseclient, "Synapse", return_value=syn
+        ).start()
+        self.patch_syn_login = patch.object(syn, "login", return_value=syn).start()
+
+    def teardown_method(self):
+        self.patch_synapseclient.stop()
+        self.patch_syn_login.stop()
+
+    def test_login_with_token(self):
         utils._login_to_synapse(token="my_auth_token")
-        patch_synapseclient.assert_called_once()
-        patch_syn_login.assert_called_once_with(authToken="my_auth_token")
+        self.patch_synapseclient.assert_called_once()
+        self.patch_syn_login.assert_called_once_with(authToken="my_auth_token")
 
-
-def test_login_no_token(syn):
-    with patch.object(
-        synapseclient, "Synapse", return_value=syn
-    ) as patch_synapseclient, patch.object(
-        syn, "login", return_value=syn
-    ) as patch_syn_login:
+    def test_login_no_token(self):
         utils._login_to_synapse(token=None)
-        patch_synapseclient.assert_called_once()
-        patch_syn_login.assert_called_once_with()
-
-
-# def test_get_config_with_invalid_file_path():
-#     with pytest.raises(FileNotFoundError, match="File not found. *"):
-#         utils._get_config(config_path="this/is/a/bad/path")
-
-
-# def test_get_config_with_parser_error():
-#     with pytest.raises(
-#         yaml.parser.ParserError, match="YAML file unable to be parsed. *"
-#     ):
-#         utils._get_config(config_path="./tests/test_assets/bad_config_parsing.yaml")
-
-
-# def test_get_config_with_scanner_error():
-#     with pytest.raises(
-#         yaml.scanner.ScannerError, match="YAML file unable to be scanned. *"
-#     ):
-#         utils._get_config(config_path="./tests/test_assets/bad_config_scanning.yaml")
-
-
-# def test_get_config_with_no_config_path():
-#     config = utils._get_config(config_path=None)
-#     assert list(config)[0] == {"destination": "syn12177492"}
-
-
-# def test_get_config_with_config_path():
-#     config = utils._get_config(config_path="./test_config.yaml")
-#     assert list(config)[0] == {"destination": "syn17015333"}
-
-
-# def test_find_config_by_name_where_name_in_config():
-#     config = [{"a": "b"}, {"c", "d"}]
-#     returned_object = utils._find_config_by_name(config=config, name="a")
-#     assert returned_object is not None
-
-
-# def test_find_config_by_name_where_name_not_in_config():
-#     config = [{"a": "b"}, {"c": "d"}]
-#     returned_object = utils._find_config_by_name(config=config, name="z")
-#     assert returned_object is None
+        self.patch_synapseclient.assert_called_once()
+        self.patch_syn_login.assert_called_once_with()
