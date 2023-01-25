2ca27c91c6e047655992863b181c669174c4f32e
Brad Macdonald <bwm03570@gmail.com>
2023-01-25 14:26:50 -0700

    adds test for standardize_column_names, removes commas from str.replace() calls

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,7 +1,51 @@
-# from agoradatatools.etl import transform
-# import pandas as pd
-# import numpy as np
-# import time
+from agoradatatools.etl import transform
+import pandas as pd
+import numpy as np
+
+
+def test_standardize_column_names():
+    df = pd.DataFrame(
+        {
+            "a#": ["test_value"],
+            "b@": ["test_value"],
+            "c&": ["test_value"],
+            "d*": ["test_value"],
+            "e^": ["test_value"],
+            "f?": ["test_value"],
+            "g(": ["test_value"],
+            "h)": ["test_value"],
+            "i%": ["test_value"],
+            "j$": ["test_value"],
+            "k#": ["test_value"],
+            "l!": ["test_value"],
+            "m/": ["test_value"],
+            "n ": ["test_value"],
+            "o-": ["test_value"],
+            "p.": ["test_value"],
+            "AAA": ["test_value"],
+        }
+    )
+    standard_df = transform.standardize_column_names(df=df)
+    assert list(standard_df.columns) == [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n_",
+        "o_",
+        "p_",
+        "aaa",
+    ]
+
 
 # df = pd.DataFrame(
 #     {'team id': [np.nan, 0, 1, 2],
