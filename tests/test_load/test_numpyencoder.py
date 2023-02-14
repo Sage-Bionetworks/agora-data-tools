b346828ab40347c7dd66cf593e03c6512dd9fc06
Brad Macdonald <bwm03570@gmail.com>
2023-02-14 14:28:09 -0700

    adds test_in_json_dumps

--- tests/test_load/test_numpyencoder.py
+++ tests/test_load/test_numpyencoder.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import json
 
 from agoradatatools.etl.load import NumpyEncoder
 
@@ -22,3 +23,16 @@ class TestNumpyEncoder(unittest.TestCase):
     def test_array(self):
         result = self.encoder.default(np.array([1,2,3]))
         self.assertEqual(result, [1,2,3])
+
+    def test_in_json_dumps(self):
+        test_data = {
+            'a': np.int64(1),
+            'b': np.float64(1.0),
+            'c': np.float64(0.00000003),
+            'd': np.array([4, 5, 6]),
+            'e': str(1), #test handling of type handled by json.JSONEncoder
+        }
+        expected = '{"a": 1, "b": 1.0, "c": 3e-08, "d": [4, 5, 6], "e": "1"}'
+        result = json.dumps(test_data, cls=NumpyEncoder)
+        assert result == expected
+        self.assertEqual(result, expected)
