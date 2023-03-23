1464ec07f06e0deddb6c461236b9ad0b03f7995a
Brad Macdonald <bwm03570@gmail.com>
2023-03-23 13:47:58 -0600

    adjusts setup

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="agoradatatools",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-)
+setup()
