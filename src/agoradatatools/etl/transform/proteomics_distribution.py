61cb0c709e728eb393667bee0d9ee5832203a760
Jaclyn Beck <jaclyn.beck@sagebase.org>
2023-09-29 17:56:16 -0700

    Updated SRM DE processing script for newest ADT changes, added SRM DE data to gene_info, proteomics, and proteomics_distribution transforms

--- src/agoradatatools/etl/transform/proteomics_distribution.py
+++ src/agoradatatools/etl/transform/proteomics_distribution.py
@@ -26,6 +26,8 @@ def transform_proteomics_distribution_data(datasets: dict) -> pd.DataFrame:
             df["type"] = "LFQ"
         elif name == "proteomics_tmt":
             df["type"] = "TMT"
+        elif name == "proteomics_srm":
+            df["type"] = "SRM"
         else:
             raise ValueError(f"Proteomics data type '{name}' not supported.")
 
