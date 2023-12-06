ae63679e23429081ca33bb8e04b597fc3c82ff87
Brad Macdonald <bwm03570@gmail.com>
2023-12-06 14:44:58 -0700

    create in range expectation instead

--- src/agoradatatools/great_expectations/gx/plugins/expectations/expect_column_values_to_have_list_length_in_range.py
+++ src/agoradatatools/great_expectations/gx/plugins/expectations/expect_column_values_to_have_list_length_in_range.py
@@ -12,52 +12,55 @@ from great_expectations.expectations.metrics import (
 
 # This class defines a Metric to support your Expectation.
 # For most ColumnMapExpectations, the main business logic for calculation will live in this class.
-class ColumnValuesListLengthLessThan(ColumnMapMetricProvider):
-    """Class definition for list length less than checking metric."""
+class ColumnValuesListLengthInRange(ColumnMapMetricProvider):
+    """Class definition for list length in range checking metric."""
 
     # This is the id string that will be used to reference your metric.
-    condition_metric_name = "column_values.list_length_less_than"
-    condition_value_keys = ("list_length_less_than",)
+    condition_metric_name = "column_values.list_length_range"
+    condition_value_keys = ("list_length_range",)
 
     # This method implements the core logic for the PandasExecutionEngine
     @column_condition_partial(engine=PandasExecutionEngine)
     def _pandas(
-        cls, column: pd.core.series.Series, list_length_less_than: int, **kwargs
+        cls, column: pd.core.series.Series, list_length_range: list, **kwargs
     ) -> bool:
         """Core logic for list length checking metric on a
         pandas execution engine.
 
         Args:
             column (pd.core.series.Series): Pandas column to be evaluated.
-            list_length_less_than (int): length the list should be less than.
+            list_length_range (list): list of length 2 containing the minimum and maximum allowed lengths.
         Returns:
             bool: Whether or not the column values have the expected list length.
         """
         return column.apply(
-            lambda x: cls._check_list_length_is_less_than(x, list_length_less_than)
+            lambda x: cls._check_list_length_in_range(x, list_length_range)
         )
 
     @staticmethod
-    def _check_list_length_is_less_than(cell: Any, list_length_less_than: int) -> bool:
+    def _check_list_length_in_range(cell: Any, list_length_range: list) -> bool:
         """Check if a cell is a list, and if it has the expected length.
 
         Args:
             cell (Any): Individual cell to be evaluated.
-            list_length_less_than (int): length the list should be less than.
-
+            list_length_range (list): list of length 2 containing the minimum and maximum allowed lengths.
         Returns:
             bool: Whether or not the cell is a list with the expected length.
         """
         if not isinstance(cell, list):
             return False
-        if len(cell) >= list_length_less_than:
-            return False
-        return True
+        if len(list_length_range) != 2:
+            raise ValueError(
+                "list_length_range must be a list of length 2 containing the minimum and maximum allowed lengths."
+            )
+        if len(cell) >= min(list_length_range) and len(cell) <= max(list_length_range):
+            return True
+        return False
 
 
 # This class defines the Expectation itself
-class ExpectColumnValuesToHaveListLengthLessThan(ColumnMapExpectation):
-    """Expect the list in column values to have a length less than a certain value."""
+class ExpectColumnValuesToHaveListLengthInRange(ColumnMapExpectation):
+    """Expect the list in column values to have a length within a certain range."""
 
     # These examples will be shown in the public gallery.
     # They will also be executed as unit tests for your Expectation.
@@ -68,17 +71,17 @@ class ExpectColumnValuesToHaveListLengthLessThan(ColumnMapExpectation):
             },
             "tests": [
                 {
-                    "title": "positive_test_with_list_length_6",
+                    "title": "positive_test_with_0_5",
                     "exact_match_out": False,
                     "include_in_gallery": True,
-                    "in": {"column": "a", "list_length_less_than": 6},
+                    "in": {"column": "a", "list_length_range": [0, 5]},
                     "out": {"success": True},
                 },
                 {
-                    "title": "negative_test_with_list_length_5",
+                    "title": "negative_test_with_7_10",
                     "exact_match_out": False,
                     "include_in_gallery": True,
-                    "in": {"column": "a", "list_length_less_than": 4},
+                    "in": {"column": "a", "list_length_range": [7, 10]},
                     "out": {"success": False},
                 },
             ],
@@ -87,10 +90,10 @@ class ExpectColumnValuesToHaveListLengthLessThan(ColumnMapExpectation):
 
     # This is the id string of the Metric used by this Expectation.
     # For most Expectations, it will be the same as the `condition_metric_name` defined in your Metric class above.
-    map_metric = "column_values.list_length_less_than"
+    map_metric = "column_values.list_length_range"
 
     # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
-    success_keys = ("list_length_less_than",)
+    success_keys = ("list_length_range",)
 
     # This dictionary contains default values for any parameters that should have default values
     default_kwarg_values = {}
@@ -122,4 +125,4 @@ class ExpectColumnValuesToHaveListLengthLessThan(ColumnMapExpectation):
 
 
 if __name__ == "__main__":
-    ExpectColumnValuesToHaveListLengthLessThan().print_diagnostic_checklist()
+    ExpectColumnValuesToHaveListLengthInRange().print_diagnostic_checklist()
