dd8d422927f102927c435996fb831e58d012e85e
Beatriz Saldana <bsaldana@w262.lan>
2024-09-26 12:13:24 -0700

    PR comment: Reverting back to using standard typing hints to prevent problems with puthon 3.8

--- src/agoradatatools/etl/transform/biomarkers.py
+++ src/agoradatatools/etl/transform/biomarkers.py
@@ -7,7 +7,7 @@ import pandas as pd
 from typing import Any
 
 
-def transform_biomarkers(datasets: dict[str, pd.DataFrame]) -> list[dict[str, Any]]:
+def transform_biomarkers(datasets: dict) -> list:
     """
     Takes dictionary of dataset DataFrames, extracts the biomarkers
     DataFrame, and transforms it into a list of dictionaries grouped by
@@ -17,7 +17,7 @@ def transform_biomarkers(datasets: dict[str, pd.DataFrame]) -> list[dict[str, An
         datasets (dict[str, pd.DataFrame]): dictionary of dataset names mapped to their DataFrame
 
     Returns:
-        list[dict]: a list of dictionaries containing biomarker data modeled after intended final JSON structure
+        list[dict[str, Any]]: a list of dictionaries containing biomarker data modeled after intended final JSON structure
     """
     biomarkers_dataset = datasets["biomarkers"]
 
