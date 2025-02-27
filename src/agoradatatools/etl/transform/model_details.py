cc0b153c3c114e6d65f9461876e48b454019c2c9
Beatriz Saldana <bsaldana@w262.lan>
2025-02-27 15:31:20 -0800

    Added the dataset to the modelad_test_config.yaml and created a model_details transform (does not wwork yet)

---
+++ src/agoradatatools/etl/transform/model_details.py
@@ -0,0 +1,16 @@
+"""
+This module contains the transformation logic for the model_details datasets.
+This is for the Model AD project.
+"""
+
+import pandas as pd
+from typing import Dict, List
+
+
+def transform_model_details(
+    datasets: Dict[str, pd.DataFrame],
+    dataset_names: List[str] = ["model_info", "allele_info", "pathology", "biomarkers", "human_transgene_allele_map"],
+) -> pd.DataFrame:
+    """
+    """
+    return None
