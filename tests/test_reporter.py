968591cd81ba019243ef27e5eb01be7cdf447f75
linglp <linglp@umich.edu>
2025-05-06 13:56:25 -0400

    update syntax in test

--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -1,7 +1,7 @@
 import pytest
 import datetime
 
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 import agoradatatools.reporter
 
@@ -63,7 +63,11 @@ class TestADTGXReporter:
 
     @patch(f"{agoradatatools.reporter.__name__}.datetime", wraps=datetime)
     def test_update_reports_before_upload(self, mock_datetime):
-        mock_datetime.datetime.now.return_value.strftime.return_value = "test_timestamp"
+        # Create a mock for the return value of datetime.now()
+        mock_now = Mock()
+        mock_now.strftime.return_value = "test_timestamp"
+        mock_datetime.datetime.now.return_value = mock_now
+        # mock_datetime.datetime.now.return_value.strftime.return_value = "test_timestamp"
 
         self.test_reporter.reports = [self.test_report]
         self.test_reporter._update_reports_before_upload()
