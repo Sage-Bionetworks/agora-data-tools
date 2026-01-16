048a59c17d644a59f3e10ebdd9cb44afb50b7692
Beatriz Saldana <bsaldana@w262.lan>
2026-01-16 11:54:22 -0700

    Added a couple of edgecases to extract_age_numeric

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -905,7 +905,8 @@ class TestExtractAgeNumeric:
             ("100 days", 100),  # Age with days
             ("", None),  # Empty string
             ("no number here", None),  # String without number
-            ("4 8 12 months", 4),  # Multiple numbers - should return first
+            ("number 10", 10),  # String with number
+            ("10 11 12", 10),  # String with multiple numbers
             (None, None),  # None input
         ],
     )
