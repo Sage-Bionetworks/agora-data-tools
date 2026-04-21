8f3f16de5b3ffc0590a11b84278a4383f6c80f43
Beatriz Saldana <bsaldana@w262.lan>
2026-04-21 11:21:56 -0700

    Simplified _check_single_rule with a single if/else statement

--- src/agoradatatools/etl/utils.py
+++ src/agoradatatools/etl/utils.py
@@ -352,9 +352,7 @@ def _check_single_rule(
         A list containing a violation message if the rule is broken, or an empty list.
     """
     counter = _RULE_VIOLATION_COUNTERS.get(rule.rule)
-    if counter is None:
-        return []
-    if (bad_count := counter(df[col_name], rule.value)) > 0:
+    if counter is not None and (bad_count := counter(df[col_name], rule.value)) > 0:
         prefix = f"In dataset '{dataset_name}', column '{col_name}': "
         value_detail = "" if rule.value is None else f" (value={rule.value!r})"
         return [f"{prefix}{bad_count} row(s) violate rule '{rule.rule}'{value_detail}."]
