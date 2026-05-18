da3485b4d7a132d1862ec87c3e9a5ff235645cdd
jaclynbeck-sage <113385948+jaclynbeck-sage@users.noreply.github.com>
2026-05-18 10:53:03 -0700

    AG-2017: normalize null values util function (#301)
    * Added normalize null util function
    
    * Changed to using normalize_null_values in model_details transform and fixed issue with None values in jax_id padding
    
    * Added normalize_null_values to the model_overview transform
    
    * Updated model_overview tests
    
    * Removed unnecessary string strip from model_overview
    
    * Removed test from model_details suite that passed when missing key allele_info fields
    
    * Pre-commit fixes
    
    * Changed zero_pad_jax_ids to be robust to None values in data, and added more robust testing
    
    * Updated comments
    
    * Undid changes to zero_pad_jax_ids, which have been split into a separate branch
    
    * Removed editing of pytest fixtures in normalize_null_values tests
    
    * Clarified a few comments
    
    * Simplified return statement in normalize_null_values
    
    * Removed handling for empty strings in jax ID padding so they throw errors
    
    * Clarified comment
    
    * Removed redundant comment
    
    * Clarified some comments
    
    * Updated documentation per PR comments
    
    * Added additional validation and tests for zero_pad_jax_ids
    
    * Added more robust missing value handling to process_genetic_info
    
    * Revert "Added more robust missing value handling to process_genetic_info"
    
    This reverts commit 72d9d0a2265da1f0e79a800890b11ceb0c924b2a.
    
    * Added test for normalizing missing values in process_genetic_info
    
    * Added test for normalizing null values in nest_fields
    
    * Cleaned up expected output for normalize_null_values tests
    
    * Fixed issues with ValueError strings in zero_pad_jax_ids
    
    * Fixed typo in test docstring
    
    * Fixed a few docstring issues in model_ad_transform_utils
    
    * Removed unnecessary comments from tests
    
    * Added replacement of pd.NA values to normalize_null_values and replaced deprecated np.NaN with np.nan
    
    * Removed editing of test fixture for build_transcriptomics_url
    
    * Removed unnecessary built_transcriptomics_url test
    
    * Clarified docstring for normalize_null_values function

--- src/agoradatatools/etl/transform/model_ad_transform_utils.py
+++ src/agoradatatools/etl/transform/model_ad_transform_utils.py
@@ -10,6 +10,8 @@ Functions:
 from typing import Any, Dict, List, Union
 import pandas as pd
 
+from agoradatatools.etl.utils import normalize_null_values
+
 
 def process_genetic_info(
     human_transgene_allele_map_df: pd.DataFrame,
@@ -19,6 +21,14 @@ def process_genetic_info(
     Processes the gene information DataFrame. If the allele is a human transgene,
     replace the ensembl_id with the human one. Each model's alleles are processed independently.
     Multiple entries are preserved for different alleles of the same gene.
+
+    This function assumes the input data has been pre-validated so that:
+        1. human_transgene_allele_map_df does not have any missing/NaN/None values in any column
+        2. model_alleles does not have any missing/NaN/None values in the "modified_gene" or "mgi_allele_id" columns,
+            which are needed for the merge. Missing values in other columns of model_alleles are allowed, and will be
+            normalized to None.
+    This is to ensure that human transgene map rows are not silently dropped during the merge due to missing values, and
+    that there are no missing gene symbols or allele IDs in the merged data frame.
 
     Args:
         human_transgene_allele_map_df (pd.DataFrame): The DataFrame containing the human transgene allele information.
@@ -47,6 +57,8 @@ def process_genetic_info(
         on=["mgi_allele_id", "gene_upper"],
         how="left",
     )
+
+    merged_df = normalize_null_values(merged_df)
 
     # Only override ensembl_id if we have a valid human_ensembl_id
     merged_df["ensembl_gene_id"] = merged_df.apply(
@@ -92,8 +104,8 @@ def build_transcriptomics_url(model_row: pd.Series) -> Union[str, None]:
     two (or more) model names to the "models=..." part of the string.
 
     The exact values that should go in "categories=..." and "models=..." are pulled from columns in model_row. If the
-    url_categories_value is "", the "categories=..." string is not added. If the url_models_value is "", the
-    "models=..." string defaults to "models=<model_name>".
+    url_categories_value is None or "", the "categories=..." string is not added. If the url_models_value is None or "",
+    the "models=..." string defaults to "models=<model_name>".
 
     The final url can have two different formats:
         "comparison/expression?models=..."
@@ -103,32 +115,32 @@ def build_transcriptomics_url(model_row: pd.Series) -> Union[str, None]:
         the categories "..." is a string like
             "RNA%2520-%2520DIFFERENTIAL%2520EXPRESSION,Tissue%2520-%2520Hippocampus,Sex%2520-%2520Females%2520%2526%2520Males"
 
-    The url will be None if there is no gene expression data for this model.
+    The url will be None if there is no transcriptomics data for this model.
 
     Args:
         model_row (pd.Series): A single row from the model_info data frame, which must contain columns "name",
-            "gene_expression", "url_categories_value", and "url_models_value". The latter two columns may be blank or
-            contain strings. "gene_expression" must be True, False, or None (which is interpreted as False).
+            "transcriptomics", "url_categories_value", and "url_models_value". The latter two columns may be None or
+            contain strings. "transcriptomics" must be True or False. It is assumed that normalize_null_values has
+            already been called on this data so that all missing values used in this function are None, not NA or empty
+            strings.
 
     Returns:
-        a string with the completed URL, or None if there is no gene expression data for the model
+        a string with the completed URL, or None if there is no transcriptomics data for the model
     """
     categories_value = (
         # Contains the "&" at the end to separate it from the models=... statement
         f"categories={model_row['url_categories_value']}&"
-        if pd.notna(model_row["url_categories_value"])
-        and len(model_row["url_categories_value"]) > 0  # must not be ""
+        if model_row["url_categories_value"]  # must not be "" or None
         else ""  # Only adds to URL if the url_categories_value is specified
     )
     models_value = (
         model_row["url_models_value"]  # A comma-separated list, if specified
-        if pd.notna(model_row["url_models_value"])
-        and len(model_row["url_models_value"]) > 0  # must not be ""
+        if model_row["url_models_value"]  # must not be "" or None
         else model_row["name"]  # A single model name if url_models_value is blank
     )
     url = (
         f"comparison/expression?{categories_value}models={models_value}"
-        if bool(model_row["transcriptomics"])
+        if model_row["transcriptomics"]
         else None
     )
     return url
