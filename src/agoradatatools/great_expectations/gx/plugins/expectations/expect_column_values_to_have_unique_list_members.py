281d2d5685560fc1296915af6aa27543f64a5060
Jessica <jessica.britton@sagebase.org>
2026-08-11 15:48:34 -0700

    Implement, test, and leverage new unique_list_members custom expectation

---
+++ src/agoradatatools/great_expectations/gx/plugins/expectations/expect_column_values_to_have_unique_list_members.py
@@ -0,0 +1,117 @@
+from typing import Any, Optional
+
+import pandas as pd
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.execution_engine import PandasExecutionEngine
+from great_expectations.expectations.expectation import ColumnMapExpectation
+from great_expectations.expectations.metrics import (
+    ColumnMapMetricProvider,
+    column_condition_partial,
+)
+
+
+# This class defines a Metric to support your Expectation.
+# For most ColumnMapExpectations, the main business logic for calculation will live in this class.
+class ColumnValuesListMembersUnique(ColumnMapMetricProvider):
+    """Class definition for list member uniqueness checking metric."""
+
+    # This is the id string that will be used to reference your metric.
+    condition_metric_name = "column_values.list_members.unique"
+
+    # This method implements the core logic for the PandasExecutionEngine
+    @column_condition_partial(engine=PandasExecutionEngine)
+    def _pandas(cls, column: pd.core.series.Series, **kwargs) -> bool:
+        """Core logic for list member uniqueness checking metric on a
+        pandas execution engine.
+
+        Args:
+            column (pd.core.series.Series): Pandas column to be evaluated.
+        Returns:
+            bool: Whether or not the column values have unique list members.
+        """
+        return column.apply(cls._check_unique_list_members)
+
+    @staticmethod
+    def _check_unique_list_members(cell: Any) -> bool:
+        """Check if a cell is a list, and if all of its members are unique.
+
+        Args:
+            cell (Any): Individual cell to be evaluated.
+
+        Returns:
+            bool: Whether or not the cell is a list with no duplicate members.
+        """
+        if not isinstance(cell, list):
+            return False
+        return len(cell) == len(set(cell))
+
+
+# This class defines the Expectation itself
+class ExpectColumnValuesToHaveUniqueListMembers(ColumnMapExpectation):
+    """Expect the list in column values to have no duplicate members."""
+
+    # These examples will be shown in the public gallery.
+    # They will also be executed as unit tests for your Expectation.
+    examples = [
+        {
+            "data": {
+                "a": [["a", "b", "c"]],
+                "b": [["a", "b", "a"]],
+            },
+            "tests": [
+                {
+                    "title": "positive_test_with_unique_members",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {"column": "a"},
+                    "out": {"success": True},
+                },
+                {
+                    "title": "negative_test_with_duplicate_members",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {"column": "b"},
+                    "out": {"success": False},
+                },
+            ],
+        }
+    ]
+
+    # This is the id string of the Metric used by this Expectation.
+    # For most Expectations, it will be the same as the `condition_metric_name` defined in your Metric class above.
+    map_metric = "column_values.list_members.unique"
+
+    # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
+    success_keys = ()
+
+    # This dictionary contains default values for any parameters that should have default values
+    default_kwarg_values = {}
+
+    def validate_configuration(
+        self, configuration: Optional[ExpectationConfiguration] = None
+    ) -> None:
+        """
+        Validates that a configuration has been set, and sets a configuration if it has yet to be set. Ensures that
+        necessary configuration arguments have been provided for the validation of the expectation.
+
+        Args:
+            configuration (OPTIONAL[ExpectationConfiguration]): \
+                An optional Expectation Configuration entry that will be used to configure the expectation
+        Returns:
+            None. Raises InvalidExpectationConfigurationError if the config is not validated successfully
+        """
+
+        super().validate_configuration(configuration)
+        configuration = configuration or self.configuration
+
+    # This object contains metadata for display in the public Gallery
+    library_metadata = {
+        "tags": [],  # Tags for this Expectation in the Gallery
+        "contributors": [  # Github handles for all contributors to this Expectation.
+            "@JessterB",  # Don't forget to add your github handle here!
+        ],
+    }
+
+
+if __name__ == "__main__":
+    ExpectColumnValuesToHaveUniqueListMembers().print_diagnostic_checklist()
