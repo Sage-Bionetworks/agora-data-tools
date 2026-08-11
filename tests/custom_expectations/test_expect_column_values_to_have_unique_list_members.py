6db14a4e3fe3791df7432519d590b0443b92ffb1
Jessica <jessica.britton@sagebase.org>
2026-08-11 16:26:17 -0700

    Pre-commit updates

--- tests/custom_expectations/test_expect_column_values_to_have_unique_list_members.py
+++ tests/custom_expectations/test_expect_column_values_to_have_unique_list_members.py
@@ -8,11 +8,15 @@ class TestColumnValuesListMembersUnique:
         assert ColumnValuesListMembersUnique._check_unique_list_members(["a", "b", "c"])
 
     def test_duplicate_list_members_returns_false(self):
-        assert not ColumnValuesListMembersUnique._check_unique_list_members(["a", "b", "a"])
+        assert not ColumnValuesListMembersUnique._check_unique_list_members(
+            ["a", "b", "a"]
+        )
 
     def test_empty_list_returns_true(self):
         assert ColumnValuesListMembersUnique._check_unique_list_members([])
 
     def test_non_list_input_returns_false(self):
-        assert not ColumnValuesListMembersUnique._check_unique_list_members("not a list")
+        assert not ColumnValuesListMembersUnique._check_unique_list_members(
+            "not a list"
+        )
         assert not ColumnValuesListMembersUnique._check_unique_list_members(None)
